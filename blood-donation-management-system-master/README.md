45bdbaa812315343da7e69d2686fbe5af02981be
tayeba-bashar <64378658+tayeba-bashar@users.noreply.github.com>
2022-03-12 13:04:09 +0600

    Add files via upload

---
+++ blood-donation-management-system-master/README.md
@@ -0,0 +1,27 @@
+# Blood Donation Management System
+
+A web application for blood donors intended to  enhance their donation experience.
+
+Functionalities include profile information update, donation eligibility check, appointment booking and management, history of donations, closest donation facility search, donation ranking in each donor’s area, and more.
+
+>Developed using MySQL, Express, Angular 4, and Node.js
+
+### Source code and mysql scripts are included in the src and sql folders. This project was developed and hosted on Cloud 9 so you can easily run the project through the Cloud 9 workspace here:  https://ide.c9.io/izvonkov/blood-donation-database
+### Follow these instructions on Cloud 9 to run the project.
+Open three terminals to run mysql, the server, and the client:
+
+in the first terminal : run mysql
+>mysql -u root
+
+in the second terminal : navigate to the "server" folder to run the server
+>cd server
+
+>node server.js
+
+in the third terminal : nagivate to the "angular-project" folder to run the client
+>cd angular-project
+
+>npm start
+
+### Finally, project will be live at this link: https://blood-donation-database-izvonkov.c9users.io:8081
+
---
+++ index.php
@@ -0,0 +1,200 @@
+<?php
+error_reporting(0);
+include('includes/config.php');
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Blood Donation Management System</title>
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="css/modern-business.css" rel="stylesheet">
+    <style>
+    .navbar-toggler {
+        z-index: 1;
+    }
+    
+    @media (max-width: 576px) {
+        nav > .container {
+            width: 100%;
+        }
+    }
+    .carousel-item.active,
+    .carousel-item-next,
+    .carousel-item-prev {
+        display: block;
+    }
+    </style>
+
+</head>
+
+<body>
+
+    <!-- Navigation -->
+<?php include('includes/header.php');?>
+<?php include('includes/slider.php');?>
+   
+    <!-- Page Content -->
+    <div class="container">
+
+        <h1 class="my-4">Welcome to Blood Donation Management System</h1>
+
+        
+										<div class="panel panel-default">
+											<div class="panel-body bk-success text-light">
+												<div class="stat-panel text-center">
+												<?php 
+
+$sql1 ="SELECT id from tblblooddonars where gender='Male'";                                 
+$query1 = $dbh -> prepare($sql1);;
+$query1->execute();
+$results1=$query1->fetchAll(PDO::FETCH_OBJ);
+$regbd=$query1->rowCount();
+
+?>
+													<div class="stat-panel-number h1 "><?php echo htmlentities($regbd);?></div>
+													<div class="stat-panel-title text-uppercase">Male Donors</div>
+												</div>
+											</div>
+                                            <div class="panel panel-default">
+											<div class="panel-body bk-success text-light">
+												<div class="stat-panel text-center">
+												<?php 
+
+$sql1 ="SELECT id from tblblooddonars where gender='Female'";                                 
+$query1 = $dbh -> prepare($sql1);;
+$query1->execute();
+$results1=$query1->fetchAll(PDO::FETCH_OBJ);
+$regbd=$query1->rowCount();
+
+?>
+													<div class="stat-panel-number h1 "><?php echo htmlentities($regbd);?></div>
+													<div class="stat-panel-title text-uppercase">Female Donors</div>
+												</div>
+											</div>
+                                                <div class="panel panel-default">
+											<div class="panel-body bk-success text-light">
+												<div class="stat-panel text-center">
+												<?php 
+
+$sql1 ="SELECT id from tblblooddonars where age>=20";                                 
+$query1 = $dbh -> prepare($sql1);;
+$query1->execute();
+$results1=$query1->fetchAll(PDO::FETCH_OBJ);
+$regbd=$query1->rowCount();
+
+?>
+													<div class="stat-panel-number h1 "><?php echo htmlentities($regbd);?></div>
+													<div class="stat-panel-title text-uppercase">Donors Above or Equal Age 20</div>
+												</div>
+											</div>
+                                                    <div class="panel panel-default">
+											<div class="panel-body bk-success text-light">
+												<div class="stat-panel text-center">
+												<?php 
+
+$sql1 ="SELECT id from tblblooddonars where age<20";                                 
+$query1 = $dbh -> prepare($sql1);;
+$query1->execute();
+$results1=$query1->fetchAll(PDO::FETCH_OBJ);
+$regbd=$query1->rowCount();
+
+?>
+													<div class="stat-panel-number h1 "><?php echo htmlentities($regbd);?></div>
+													<div class="stat-panel-title text-uppercase">Donors Under Age 20</div>
+												</div>
+											</div>
+
+        <!-- Portfolio Section -->
+        <h2>Some of the Donors</h2>
+
+        <div class="row">
+                   <?php 
+$status=1;
+$sql = "SELECT * from tblblooddonars where status=:status order by rand() limit 6";
+$query = $dbh -> prepare($sql);
+$query->bindParam(':status',$status,PDO::PARAM_STR);
+$query->execute();
+$results=$query->fetchAll(PDO::FETCH_OBJ);
+$cnt=1;
+if($query->rowCount() > 0)
+{
+foreach($results as $result)
+{ ?>
+
+            <div class="col-lg-4 col-sm-6 portfolio-item">
+                <div class="card h-100">
+                    <a href="#"><img class="card-img-top img-fluid" src="images/blood-donor.jpg" alt="" ></a>
+                    <div class="card-block">
+                        <h4 class="card-title"><a href="#"><?php echo htmlentities($result->FullName);?></a></h4>
+<p class="card-text"><b>  Gender :</b> <?php echo htmlentities($result->Gender);?></p>
+<p class="card-text"><b>Blood Group :</b> <?php echo htmlentities($result->BloodGroup);?></p>
+
+                    </div>
+                </div>
+            </div>
+
+            <?php }} ?>
+          
+ 
+
+
+
+        </div>
+        <!-- /.row -->
+
+        <!-- Features Section -->
+        <div class="row">
+            <div class="col-lg-6">
+                <h2>BLOOD GROUPS</h2>
+          <p>  Blood group of any human being will mainly fall in any one of the following groups.</p>
+                <ul>
+                
+                
+<li>A positive or A negative</li>
+<li>B positive or B negative</li>
+<li>O positive or O negative</li>
+<li>AB positive or AB negative.</li>
+                </ul>
+                <p>A healthy diet helps ensure a successful blood donation, and also makes you feel better! Check out the following recommended foods to eat prior to your donation.</p>
+            </div>
+        </div>
+        <!-- /.row -->
+
+        <hr>
+
+        <!-- Call to Action Section -->
+        <div class="row mb-4">
+            <div class="col-md-8">
+            <h4>UNIVERSAL DONORS AND RECIPIENTS</h4>
+                <p>
+The most common blood type is O, followed by type A.
+
+Type O individuals are often called "universal donors" since their blood can be transfused into persons with any blood type. Those with type AB blood are called "universal recipients" because they can receive blood of any type.</p>
+            </div>
+            <div class="col-md-4">
+                <a class="btn btn-lg btn-secondary btn-block" href="become-donar.php">Become a Donar</a>
+            </div>
+        </div>
+
+    </div>
+    <!-- /.container -->
+
+    <!-- Footer -->
+  <?php include('includes/footer.php');?>
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/tether/tether.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
